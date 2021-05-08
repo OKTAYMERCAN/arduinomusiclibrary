45f61b394cb06a03f4b0062e3174ed6b63834a4f
Oktay Mercan <oktaylamacera@gmail.com>
2021-05-08 21:45:02 +0300

    Add files via upload

---
+++ Pac_Man/Pac_Man.ino
@@ -0,0 +1,95 @@
+int tonePin = 11;
+void setup() {}
+
+void midi() {
+
+    tone(tonePin, 493, 70.75475);
+    delay(70.75475);
+    delay(70.75475);
+    tone(tonePin, 987, 70.75475);
+    delay(70.75475);
+    delay(70.75475);
+    tone(tonePin, 739, 70.75475);
+    delay(70.75475);
+    delay(70.75475);
+    tone(tonePin, 622, 70.75475);
+    delay(70.75475);
+    delay(70.75475);
+    tone(tonePin, 987, 70.75475);
+    delay(70.75475);
+    tone(tonePin, 739, 70.75475);
+    delay(70.75475);
+    delay(141.5095);
+    tone(tonePin, 622, 212.26425);
+    delay(212.26425);
+    delay(70.75475);
+    tone(tonePin, 523, 70.75475);
+    delay(70.75475);
+    delay(70.75475);
+    tone(tonePin, 1046, 70.75475);
+    delay(70.75475);
+    delay(70.75475);
+    tone(tonePin, 783, 70.75475);
+    delay(70.75475);
+    delay(70.75475);
+    tone(tonePin, 659, 70.75475);
+    delay(70.75475);
+    delay(70.75475);
+    tone(tonePin, 1046, 70.75475);
+    delay(70.75475);
+    tone(tonePin, 783, 70.75475);
+    delay(70.75475);
+    delay(141.5095);
+    tone(tonePin, 659, 212.26425);
+    delay(212.26425);
+    delay(70.75475);
+    tone(tonePin, 493, 70.75475);
+    delay(70.75475);
+    delay(70.75475);
+    tone(tonePin, 987, 70.75475);
+    delay(70.75475);
+    delay(70.75475);
+    tone(tonePin, 739, 70.75475);
+    delay(70.75475);
+    delay(70.75475);
+    tone(tonePin, 622, 70.75475);
+    delay(70.75475);
+    delay(70.75475);
+    tone(tonePin, 987, 70.75475);
+    delay(70.75475);
+    tone(tonePin, 739, 70.75475);
+    delay(70.75475);
+    delay(141.5095);
+    tone(tonePin, 622, 212.26425);
+    delay(212.26425);
+    delay(70.75475);
+    tone(tonePin, 622, 70.75475);
+    delay(70.75475);
+    tone(tonePin, 659, 70.75475);
+    delay(70.75475);
+    tone(tonePin, 698, 70.75475);
+    delay(70.75475);
+    delay(70.75475);
+    tone(tonePin, 698, 70.75475);
+    delay(70.75475);
+    tone(tonePin, 739, 70.75475);
+    delay(70.75475);
+    tone(tonePin, 783, 70.75475);
+    delay(70.75475);
+    delay(70.75475);
+    tone(tonePin, 783, 70.75475);
+    delay(70.75475);
+    tone(tonePin, 830, 70.75475);
+    delay(70.75475);
+    tone(tonePin, 880, 70.75475);
+    delay(70.75475);
+    delay(70.75475);
+    tone(tonePin, 987, 141.5095);
+    delay(141.5095);
+
+}
+
+void loop() {
+    // Play midi
+    midi();
+}
